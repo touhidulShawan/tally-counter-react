b62329625151f6cdc54887036a97777d3251a028
touhidulShawan <touhidulislamshaon@gmail.com>
2019-05-01 01:28:38 +0600

    Updates

---
+++ precache-manifest.d219aa0212f6d73ed602f71ecf97799b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "472d0deaca698f9621b34341bcf01c9d",
+    "url": "/tally-counter-react/index.html"
+  },
+  {
+    "revision": "3da040e9b552b0c03adf",
+    "url": "/tally-counter-react/static/css/main.eedd54dd.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/tally-counter-react/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "3da040e9b552b0c03adf",
+    "url": "/tally-counter-react/static/js/main.3f330b7d.chunk.js"
+  },
+  {
+    "revision": "a98d1b54d5123d6b16a2",
+    "url": "/tally-counter-react/static/js/runtime~main.d6bd247e.js"
+  }
+]);
